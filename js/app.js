df11e7b76065a409c1e45d0f4bdd238f02a8f0ce
kankkuna <karina842@hotmail.com>
2017-12-27 18:36:18 -0500

    estructura general html css y js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  setTimeout(function(){
+    $('#splash').fadeOut(700);}, 2000);
+
+    /*setTimeout(function(){
+      $('#splash2').fadeOut(700);}, 5000);*/
+});
+
+function newDoc() {
+  window.location.assign('index1.html')
+}
---
+++ js/app1.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('ul.listado li a').click(function() {
+    var textoFiltro = $(this).text().toLowerCase().replace(/\s/g,"-");
+    if(textoFiltro == 'todos'){
+      $('div.filtros div.hidden').fadeIn('slow').removeClass('hidden');
+    }else{
+      $('.filtros div').each(function(){
+        if(!$(this).hasClass(textoFiltro)){
+          $(this).fadeOut('normal').addClass('hidden');
+        }else{
+          $(this).fadeIn('slow').removeClass('hidden');
+        }
+      });
+    } return false;
+  });
+});
